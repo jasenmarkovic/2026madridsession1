4742f2e4684a0ef4f87ca15d88771cb074507e35
Jasen <jasenmarkovic88@gmail.com>
2026-01-16 11:32:01 +0100

    First ever commit

---
+++ Session 1.py
@@ -0,0 +1,7 @@
+print('hello world')
+print('hello world 2')
+print('the man asked: "how are you?"')
+print(5+9)
+print("5+9 =", 5+9)
+print("5+9 = ", 5+9, ". This was easy, right?", sep="", end=". What is this?")
+
